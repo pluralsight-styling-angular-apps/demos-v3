a9084db5b41da6dc7c2b3b8183379529be296ded
Brian.Treese <brian.treese@socreate.it>
2023-12-10 16:52:45 -0800

    remove update scripts

--- angular-course-commit.ps1
+++
@@ -1,6 +0,0 @@
-Write-Host "`n============== Staging Files =============="
-git add .
-
-Write-Host "`n============== Commiting Files =============="
-# git commit -m "update to angular 16"
-git commit -m "update gitignore"
