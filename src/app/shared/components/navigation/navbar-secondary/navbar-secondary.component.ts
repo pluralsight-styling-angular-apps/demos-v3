6410511a934c4ae4dd7e7556bb73749895fcc1b6
Brian.Treese <brian.treese@socreate.it>
2024-12-06 19:19:42 -0800

    update to angular 19

--- src/app/shared/components/navigation/navbar-secondary/navbar-secondary.component.ts
+++ src/app/shared/components/navigation/navbar-secondary/navbar-secondary.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'saa-navbar-secondary',
-  templateUrl: './navbar-secondary.component.html',
-  styleUrls: ['./navbar-secondary.component.scss']
+    selector: 'saa-navbar-secondary',
+    templateUrl: './navbar-secondary.component.html',
+    styleUrls: ['./navbar-secondary.component.scss'],
+    standalone: false
 })
 
 export class NavBarSecondaryComponent { }
