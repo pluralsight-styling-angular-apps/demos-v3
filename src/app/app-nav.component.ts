299a70bb5d71b1c387cf8a0ad198871ae1a78093
Brian.Treese <brian.treese@socreate.it>
2024-12-06 18:04:07 -0800

    update to angular 19

--- src/app/app-nav.component.ts
+++ src/app/app-nav.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'saa-app-nav',
     templateUrl: './app-nav.component.html',
-    styleUrls: ['./app-nav.component.css']
+    styleUrls: ['./app-nav.component.css'],
+    standalone: false
 })
 
 export class AppNavComponent {
