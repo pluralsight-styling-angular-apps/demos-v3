30513ff4dfa6db0025cfa64809b02da3cbefcef3
Brian.Treese <brian.treese@socreate.it>
2024-12-06 18:48:29 -0800

    update to angular 19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@ import { Component, ViewEncapsulation } from '@angular/core';
 @Component({
     selector: 'saa-app',
     templateUrl: './app.component.html',
-    encapsulation: ViewEncapsulation.None
+    encapsulation: ViewEncapsulation.None,
+    standalone: false
 })
 
 export class AppComponent {
