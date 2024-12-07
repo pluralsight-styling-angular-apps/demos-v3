66de2f2f301ebc87be0cac2be479c5cf810d90bb
Brian.Treese <brian.treese@socreate.it>
2024-12-06 21:54:10 -0800

    update to angular 19

--- src/app/shared/components/content/thumbnail-list/thumbnail-list.component.ts
+++ src/app/shared/components/content/thumbnail-list/thumbnail-list.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'saa-thumbnail-list',
     templateUrl: './thumbnail-list.component.html',
-    styleUrls: ['./thumbnail-list.component.scss']
+    styleUrls: ['./thumbnail-list.component.scss'],
+    standalone: false
 })
 
 export class ThumbnailListComponent { }
