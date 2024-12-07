d4361b214ba0aa77ab918a07c22edec52412ed2a
Brian.Treese <brian.treese@socreate.it>
2024-12-06 19:06:36 -0800

    update to angular 19

--- src/app/shared/components/navigation/navbar-primary/navbar-primary.component.ts
+++ src/app/shared/components/navigation/navbar-primary/navbar-primary.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'saa-navbar-primary',
-  templateUrl: './navbar-primary.component.html',
-  styleUrls: ['./navbar-primary.component.scss']
+    selector: 'saa-navbar-primary',
+    templateUrl: './navbar-primary.component.html',
+    styleUrls: ['./navbar-primary.component.scss'],
+    standalone: false
 })
 
 export class NavBarPrimaryComponent { }
