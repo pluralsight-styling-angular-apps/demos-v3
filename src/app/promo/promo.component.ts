d8de46b6ed62d205ac0e03d0c48a7cb269d36d3e
Brian.Treese <brian.treese@socreate.it>
2024-12-06 22:03:21 -0800

    update to angular 19

--- src/app/promo/promo.component.ts
+++ src/app/promo/promo.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'saa-promo',
     templateUrl: './promo.component.html',
-    styleUrls: ['./promo.component.scss']
+    styleUrls: ['./promo.component.scss'],
+    standalone: false
 })
 
 export class PromoComponent {
