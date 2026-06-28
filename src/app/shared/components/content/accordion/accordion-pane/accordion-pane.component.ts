1bdcfd2a52e242df3271516e5fdd14fb62938b1b
Brian Treese <Brian.Treese@SoCreate.it>
2026-06-28 10:49:57 -0700

    update to angular 22

--- src/app/shared/components/content/accordion/accordion-pane/accordion-pane.component.ts
+++ src/app/shared/components/content/accordion/accordion-pane/accordion-pane.component.ts
@@ -1,13 +1,14 @@
-import { Component, TemplateRef, input, viewChild } from '@angular/core';
+import { Component, TemplateRef, input, viewChild, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
     selector: 'saa-accordion-pane',
     templateUrl: './accordion-pane.component.html',
     styleUrls: ['./accordion-pane.component.scss'],
+    changeDetection: ChangeDetectionStrategy.Eager,
     standalone: false
 })
 
 export class AccordionPaneComponent {
     readonly template = viewChild(TemplateRef);
-    readonly title = input<string>(undefined);
+    readonly title = input<string | undefined>(undefined);
 }
